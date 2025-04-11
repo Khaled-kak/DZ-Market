6961c384007634e93b82a0e8a9490791ce6bed82
Khaled-kak <kaklipro@gmail.com>
2025-04-11 21:49:01 +0200

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,17 +6,17 @@ export function HomePage() {
     {
       id: '1',
       titre: 'Huile d’olive Kabyle 1L',
-      image: 'https://images.unsplash.com/photo-1600185365929-3f0495c31e65?auto=format&fit=crop&w=600&q=80',
+      image: 'https://source.unsplash.com/600x400/?olive-oil',
     },
     {
       id: '2',
       titre: 'Dattes Deglet Nour',
-      image: 'https://images.unsplash.com/photo-1623088643162-c5d4145589e1?auto=format&fit=crop&w=600&q=80',
+      image: 'https://source.unsplash.com/600x400/?dates',
     },
     {
       id: '3',
       titre: 'Couscous artisanal',
-      image: 'https://images.unsplash.com/photo-1605518327286-753c225bfb46?auto=format&fit=crop&w=600&q=80',
+      image: 'https://source.unsplash.com/600x400/?couscous',
     },
   ];
 
